8ff61e392d09e7cc62f00becbe315c2feb45c778
VictorKunkel <victorkunkell@gmail.com>
2023-05-25 17:51:44 -0300

    HTML and CSS finished

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let runningTotal = 0;
+let buffer = 0;
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick (value) {
+    if(isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
